7b22d1a44aa374bbd40f0a9010c7b1828a2f596d
MeloAguilar <carmelo.aguilar@iesnervion.es>
2022-10-21 23:19:27 +0200

    corregidox3

--- app/src/main/java/com/example/miprimeraapp/CheckBoxesActivity.kt
+++ app/src/main/java/com/example/miprimeraapp/CheckBoxesActivity.kt
@@ -7,8 +7,12 @@ import android.util.TypedValue
 import android.widget.Button
 import android.widget.EditText
 import android.widget.RadioButton
+import androidx.core.content.getSystemService
 import com.example.miprimeraapp.databinding.ActivityCheckBoxesBinding
+import com.google.android.material.checkbox.MaterialCheckBox
+import com.google.android.material.color.MaterialColors
 import com.google.android.material.tabs.TabLayout.Mode
+import com.google.android.material.theme.overlay.MaterialThemeOverlay
 
 class CheckBoxesActivity : PintorMenuActivity() {
 
@@ -24,38 +28,45 @@ class CheckBoxesActivity : PintorMenuActivity() {
 
         //Declaro e instancio el boton de reinicio, los radio y el texto que escribirá el usuario
         val texto: EditText = findViewById(R.id.txtModificar)
-        val btnAgrandar: RadioButton = findViewById(R.id.btnLetraGigante)
-        val btnDisminuir: RadioButton = findViewById(R.id.btnLetraMinuscula)
-        val btnBold: RadioButton = findViewById(R.id.btnNegrita)
-        val btnRojo: RadioButton = findViewById(R.id.btnColorRojo)
-        val btnReiniciar : Button = findViewById(R.id.btnReiniciar)
+        val btnAgrandar: MaterialCheckBox = findViewById(R.id.btnLetraGigante)
+        val btnDisminuir: MaterialCheckBox = findViewById(R.id.btnLetraMinuscula)
+        val btnBold: MaterialCheckBox = findViewById(R.id.btnNegrita)
+        val btnRojo: MaterialCheckBox = findViewById(R.id.btnColorRojo)
+        val btnReiniciar: Button = findViewById(R.id.btnReiniciar)
 
         //Establezco que si los radio no estan seleccionados estos se seleccionen y
         btnAgrandar?.setOnClickListener {
-            if (!btnAgrandar.isSelected) {
-                btnAgrandar.isSelected = true
+
+            if (btnDisminuir.isChecked) {
+                btnDisminuir.isChecked = false
+            }
+                btnAgrandar.isChecked = true
                 texto.setTextSize(TypedValue.COMPLEX_UNIT_SP, 100F)
-            }
+
+
         }
-            btnDisminuir?.setOnClickListener {
-                if (!btnDisminuir.isSelected) {
-                    btnDisminuir.isSelected = true
-                    texto.setTextSize(TypedValue.COMPLEX_UNIT_SP, 5F)
-                }
-            }
+        btnDisminuir?.setOnClickListener {
+            if(btnAgrandar.isChecked)
+                btnAgrandar.isChecked = false
+
+                btnDisminuir.isChecked = true
+                texto.setTextSize(TypedValue.COMPLEX_UNIT_SP, 10F)
+
+        }
         btnRojo?.setOnClickListener {
-            if (!btnRojo.isSelected) {
-                btnRojo.isSelected = true
+
+                btnRojo.isChecked = true
                 texto.setTextColor(Color.RED)
-            }
+
         }
+
 
 
         btnBold?.setOnClickListener {
-            if(!btnBold.isSelected){
-                btnBold.isSelected = true
+
+                btnBold.isChecked = true
                 texto.setTypeface(null, Typeface.BOLD)
-            }
+
         }
 
         //Boton que se encarga de reiniciar el texto ya que no entiendo como deseleccionar un radio
@@ -68,7 +79,6 @@ class CheckBoxesActivity : PintorMenuActivity() {
             texto.setTextColor(Color.BLACK)
             texto.setTypeface(null, Typeface.NORMAL)
         }
-
 
 
     }
